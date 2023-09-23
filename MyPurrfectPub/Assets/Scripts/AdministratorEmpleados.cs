704fa4582bf1c35cdf5ea62408a2142f16163064
Laia <laia.fustercorral@gmail.com>
2023-09-23 21:10:30 +0200

    Reseteo de la contratación de empleados al iniciar partida

--- MyPurrfectPub/Assets/Scripts/AdministratorEmpleados.cs
+++ MyPurrfectPub/Assets/Scripts/AdministratorEmpleados.cs
@@ -9,21 +9,15 @@ public class AdministratorEmpleados : MonoBehaviour
     [SerializeField] private List<EmployeeSO> lstEmployee;
     [SerializeField] private Transform panel;
     [SerializeField] private GameObject PanelPrefab;
-    // Start is called before the first frame update
+
     void Start()
     {
         foreach (EmployeeSO employee in lstEmployee)
         {
+            employee.Reset();
+
             GameObject newPanel = Instantiate(PanelPrefab, panel);
             newPanel.GetComponent<PanelEmployee>().AsignarValoresEmpleado(employee);
-            
-            //newButton.onClick.AddListener()
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
